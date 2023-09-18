5d10afe155b20bdb057631406c7792c6199a0dc6
junsikBang <jyjsbang@gmail.com>
2023-09-18 11:23:21 +0900

    타이머 수정, 안내멘트박스 추가

--- js/1hourLotto.js
+++ js/1hourLotto.js
@@ -1,4 +1,6 @@
 const lotto_box = document.getElementsByClassName('lotto_box')[0];
+const blur = document.getElementsByClassName('blur_box')[0];
+const comment = document.getElementsByClassName('comment')[0];
 let colors = ['red', 'yellow', 'blue', 'white'];
 let i = 0;
 
@@ -6,20 +8,31 @@ let hour = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
 
-let m = 59
-let s = 5;
+let today = new Date();
+let current_min = today.getMinutes();
+let currnet_sec = today.getSeconds();
+
+let m = 59 - current_min;
+let s = 59 - currnet_sec;
 
 window.addEventListener('DOMContentLoaded', function()
 {
     this.setInterval(function(){
         lotto_box.style.borderColor = colors[i++];
         i = i % 4;
+        comment.classList.toggle('dis_none');
     }, 2000);
+
+    min.innerText = m;
 
     this.setInterval(function(){
         if(s < 0) {
             s = 59;
             m--;
+            if(m < 0) {
+                m = 59 - current_min;
+                blur.style.display = 'none';
+            }
             min.innerText = m;
         }
         sec.innerText = s--;
