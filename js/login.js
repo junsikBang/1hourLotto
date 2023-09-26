8bd2a735e36c0a9935f82a8248311299ad2194a7
junsikBang <jyjsbang@gmail.com>
2023-09-26 14:18:29 +0900

    id, pwd 판별기능 추가

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
 const loginBtn = document.getElementById('loginBtn');
+const input_id = document.getElementById('id');
+const input_pwd = document.getElementById('pwd');
+
+let id = ['admin'];
+let pwd = ['1234'];
 
 loginBtn.addEventListener('click', () => {
-    let link = 'index.html';
-    location.href = link;
+    if(input_id.value == id[0] && input_pwd.value == pwd[0]) {
+        let link = 'index.html';
+        location.href = link;
+    }
+    else {
+        alert('아이디 또는 비밀번호가 일치하지 않습니다.'); 
+        input_id.value = null;
+        input_pwd.value = null;
+    }
 });
