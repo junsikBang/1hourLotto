6cb2f1b6568a3edef5789a214f36d1908737ba2a
junsikBang <jyjsbang@gmail.com>
2023-10-11 16:17:46 +0900

    구매 페이지: 자동구매: 추첨버튼 기능 추가

--- js/buy.js
+++ js/buy.js
@@ -1,10 +1,57 @@
 const lotteryBtn = document.getElementById('lottery_btn');
 const numNodeList = document.getElementsByName('buy_num');
+const auto_box = document.getElementById('auto_lotto_box_list');
+
+
+let lotto;
+let lottoArr = new Array();
+
+let lottery_num_arr = new Array(5);
+for(let i = 0; i < lottery_num_arr.length; i++) {
+    lottery_num_arr[i] = new Array(6);
+}
 
 lotteryBtn.addEventListener('click', () => {
+    removeAllchild(auto_box);
+
     numNodeList.forEach((node) => {
         if(node.checked) {
-            console.log(node.value);
+            for(let i = 0; i < node.value; i++) {
+                for(let j = 0; j < 6; j++) {
+                    lotto = Math.floor(Math.random() * 45) + 1;
+            
+                    while(true) {
+                        if(lottoArr.indexOf(lotto) < 0) {
+                            lottoArr[j] = lotto;
+                            break;
+                        }
+                        else {
+                            lotto = Math.floor(Math.random() * 45) + 1;
+                        }
+                    }
+                }
+            
+                lottoArr.sort(function(a, b){
+                    return a - b;
+                });
+
+                
+                for(let k =0; k < 6; k++) {
+                    lottery_num_arr[i][k] = lottoArr[k];
+                }
+
+                let str = lottery_num_arr[i].join(" ");
+
+                let plusLi = document.createElement('li');
+                plusLi.innerHTML = "<li>" + str + "</li>";
+                auto_box.prepend(plusLi);
+            }
         }
     })
 })
+
+function removeAllchild(div) {
+    while (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);
+    }
+}
